85132bcf76461884f534819f0b07905a2bb6fcb1
portal2312 <portal2312@gmail.com>
2024-08-21 00:57:10 +0900

    Update project.settings

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rz5*e!r+dnj+y#*++s=4g$uw#of85o+^n_-z7mi#_@47#euh!i"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-rz5*e!r+dnj+y#*++s=4g$uw#of85o+^n_-z7mi#_@47#euh!i",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -49,8 +53,8 @@ INSTALLED_APPS = [
     # https://strawberry.rocks/docs/integrations/django#async-django
     "strawberry_django",
     # Local apps.
-    "app",
     "utils",
+    "app",
 ]
 
 MIDDLEWARE = [
