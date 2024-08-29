4b9ee862e9716896e22195bb3f0e0c4c99a3da64
portal2312 <portal2312@gmail.com>
2024-08-29 17:03:27 +0900

    Add Parameter in my_pydantic.models

--- my_pydantic/models.py
+++ my_pydantic/models.py
@@ -1,1 +1,40 @@
 """Models in the my_pydantif app."""
+
+from pydantic import BaseModel, Field, model_validator
+from pydantic_core import PydanticCustomError
+
+
+class Parameter(BaseModel):
+    """Parameter."""
+
+    preferred_lifetime: int | None = Field(
+        default=None,
+        gt=0,
+        description="Preferred Lifetime",
+    )
+    valid_lifetime: int | None = Field(
+        default=None,
+        gt=0,
+        description="Valid Lifetime",
+    )
+
+    @model_validator(mode="after")
+    def check_preferred_lifetime__lte__valid_lifetime(self):
+        """Check preferred_lifetime less than or equal valid_lifetime."""
+        if self.preferred_lifetime is None:
+            return self
+        if self.valid_lifetime is None:
+            raise PydanticCustomError(
+                "preferred_lifetime__lte__valid_lifetime__is_none",
+                "Preferred Lifetime 의 값이 있는 경우, Valid Lifetime 의 값이 존재해야 합니다.",
+            )
+        if self.preferred_lifetime > self.valid_lifetime:
+            raise PydanticCustomError(
+                "preferred_lifetime__lte__valid_lifetime__greater",
+                "Preferred Lifetime 이 Valid Lifetime 보다 큽니다: {preferred_lifetime} <= {valid_lifetime} 은 올바르지 않습니다.",
+                {
+                    "preferred_lifetime": self.preferred_lifetime,
+                    "valid_lifetime": self.valid_lifetime,
+                },
+            )
+        return self
--- my_pydantic/tests.py
+++ my_pydantic/tests.py
@@ -1,1 +1,133 @@
 """Tests in the my_pydantic app."""
+
+from django.test import TestCase
+from pydantic import ValidationError
+
+from .models import Parameter
+
+
+class ParameterTestCase(TestCase):
+    """Parameter TestCase."""
+
+    def setUp(self):
+        """Set up."""
+        self.preferred_lifetime = 1
+        self.preferred_lifetimes = (self.preferred_lifetime, None)
+        self.invalid_preferred_lifetime = "a"
+        self.invalid_preferred_lifetime__gt = 0
+        self.valid_lifetime = 1
+        self.valid_lifetimes = (self.valid_lifetime, None)
+        self.invalid_valid_lifetime = "a"
+        self.invalid_valid_lifetime__gt = 0
+        self.invalid_check_preferred_lifetime__lte__valid_lifetime = (
+            self.valid_lifetime + 1
+        )
+
+    def test_preferred_lifetime(self):
+        """Test valid preferred_lifetime value."""
+        for value in self.preferred_lifetimes:
+            parameter = Parameter(
+                preferred_lifetime=value,
+                valid_lifetime=self.valid_lifetime,
+            )
+            self.assertEqual(parameter.preferred_lifetime, value)
+
+    def test_invalid_preferred_lifetime(self):
+        """Test invalid preferred_lifetime."""
+        try:
+            Parameter(
+                preferred_lifetime=self.invalid_preferred_lifetime,
+                valid_lifetime=self.valid_lifetime,
+            )
+        except ValidationError as e:
+            for error in e.errors():
+                match error["type"], error["loc"]:
+                    case "int_parsing", ("preferred_lifetime",):
+                        self.assertNotIsInstance(error["input"], int)
+                    case _:
+                        raise e
+
+    def test_invalid_preferred_lifetime__gt(self):
+        """Test invalid preferred_lifetime greater than."""
+        try:
+            Parameter(
+                preferred_lifetime=self.invalid_preferred_lifetime__gt,
+                valid_lifetime=self.valid_lifetime,
+            )
+        except ValidationError as e:
+            for error in e.errors():
+                match error["type"], error["loc"]:
+                    case "greater_than", ("preferred_lifetime",):
+                        self.assertGreaterEqual(error["input"], error["ctx"]["gt"])
+                    case _:
+                        raise e
+
+    def test_valid_lifetime(self):
+        """Test valid valid_lifetime."""
+        for value in self.valid_lifetimes:
+            try:
+                parameter = Parameter(
+                    preferred_lifetime=self.preferred_lifetime,
+                    valid_lifetime=value,
+                )
+            except ValidationError as e:
+                for error in e.errors():
+                    match error["type"]:
+                        case "preferred_lifetime__lte__valid_lifetime__is_none":
+                            self.assertIsNone(value)
+                        case _:
+                            raise e
+            else:
+                self.assertEqual(parameter.valid_lifetime, value)
+
+    def test_invalid_valid_lifetime(self):
+        """Test invalid valid_lifetime."""
+        try:
+            Parameter(
+                preferred_lifetime=self.preferred_lifetime,
+                valid_lifetime=self.invalid_valid_lifetime,
+            )
+        except ValidationError as e:
+            for error in e.errors():
+                match error["type"], error["loc"]:
+                    case "int_parsing", ("valid_lifetime",):
+                        self.assertNotIsInstance(error["input"], int)
+                    case _:
+                        raise e
+
+    def test_invalid_valid_lifetime__gt(self):
+        """Test invalid valid_lifetime greater than."""
+        try:
+            Parameter(
+                preferred_lifetime=self.preferred_lifetime,
+                valid_lifetime=self.invalid_valid_lifetime__gt,
+            )
+        except ValidationError as e:
+            for error in e.errors():
+                match error["type"], error["loc"]:
+                    case "greater_than", ("valid_lifetime",):
+                        self.assertGreaterEqual(error["input"], error["ctx"]["gt"])
+                    case _:
+                        raise e
+
+    def test_check_preferred_lifetime__lte__valid_lifetime(self):
+        """Test Parameter.check_preferred_lifetime__lte__valid_lifetime func."""
+        try:
+            parameter = Parameter(
+                preferred_lifetime=self.invalid_check_preferred_lifetime__lte__valid_lifetime,
+                valid_lifetime=self.valid_lifetime,
+            )
+        except ValidationError as e:
+            for error in e.errors():
+                match error["type"]:
+                    case "preferred_lifetime__lte__valid_lifetime__greater":
+                        self.assertGreater(
+                            self.invalid_check_preferred_lifetime__lte__valid_lifetime,
+                            self.valid_lifetime,
+                        )
+                    case "preferred_lifetime__lte__valid_lifetime__is_none":
+                        self.assertIsNone(self.valid_lifetime)
+                    case _:
+                        raise e
+        else:
+            self.assertIsInstance(parameter, Parameter)
